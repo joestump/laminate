7533d0283f658aaf5e93b017be5f96d021c1581e
Joe Stump <joe@joestump.net>
2015-01-20 16:09:19 -0800

    Remove the .js from requires.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var Laminate = {};
 
-Laminate.Badge = require('./components/badge.js');
-Laminate.Button = require('./components/button.js');
-Laminate.Card = require('./components/card.js');
-Laminate.Content = require('./components/content.js');
-Laminate.Icon = require('./components/icon.js');
-Laminate.Modal = require('./components/modal.js');
-Laminate.TitleBar = require('./components/title-bar.js');
-Laminate.Toggle = require('./components/toggle.js');
+Laminate.Badge = require('./components/badge');
+Laminate.Button = require('./components/button');
+Laminate.Card = require('./components/card');
+Laminate.Content = require('./components/content');
+Laminate.Icon = require('./components/icon');
+Laminate.InputGroup = require('./components/input-group');
+Laminate.Modal = require('./components/modal');
+Laminate.TitleBar = require('./components/title-bar');
+Laminate.Toggle = require('./components/toggle');
