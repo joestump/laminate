90d43f05072b261ed05be6f5815bb87f81b0c4c2
Joe Stump <joe@joestump.net>
2015-01-22 12:01:32 -0800

    Added a second test. The test runner only reports 1 test per file though. Weird.

--- __tests__/components/toggle-tests.js
+++ __tests__/components/toggle-tests.js
@@ -1,10 +1,26 @@
 jest.dontMock('../../src/components/toggle');
 
 var assert = require('assert');
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
 
 describe('Toggle', function() {
+  var Toggle;
+
+  beforeEach(function() {
+    Toggle = require('../../src/components/toggle');
+  });
+
+  it('is a Toggle element', function() {
+    var onToggle = function(toggled) {
+
+    };
+
+    var toggle = (<Toggle onToggle={onToggle} />);
+    expect(TestUtils.isElementOfType(toggle, Toggle)).toEqual(true);
+  });
+
   it('is defined', function() {
-    var Toggle = require('../../src/components/toggle');
     expect(Toggle).toBeDefined();
   });
 });
