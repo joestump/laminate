6ed54e23fbff68f718f7094f40dbc5ad32d0f35a
Joe Stump <joe@joestump.net>
2015-01-23 12:08:12 -0800

    Added a more interesting test checking properties and talked to Sam about state vs. properties.

--- __tests__/components/toggle-tests.js
+++ __tests__/components/toggle-tests.js
@@ -1,26 +1,38 @@
 jest.dontMock('../../src/components/toggle');
 
 var assert = require('assert');
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
 
 describe('Toggle', function() {
-  var Toggle;
+  var Toggle, onToggle, React, TestUtils;
 
   beforeEach(function() {
+    // Include React and TestUtils here because React can get double-included.
+    // This is due to various auto-mocking magic with Jest and require.
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
     Toggle = require('../../src/components/toggle');
+    onToggle = function(toggled) {
+
+    };
   });
 
   it('is a Toggle element', function() {
-    var onToggle = function(toggled) {
-
-    };
-
-    var toggle = (<Toggle onToggle={onToggle} />);
+    var toggle = <Toggle onToggle={onToggle} />;
     expect(TestUtils.isElementOfType(toggle, Toggle)).toEqual(true);
   });
 
   it('is defined', function() {
     expect(Toggle).toBeDefined();
+  });
+
+  it('defaults toggled to false', function() {
+    // JSX doesn't really return an actual instance of Toggle. Only once the
+    // component is rendered into the DOM does React fully hydrate an instance
+    // of Toggle.
+    var toggle = TestUtils.renderIntoDocument(<Toggle onToggle={onToggle} />);
+
+    // At this point the React component lifecycle has ran, including the
+    // getDefaultProps() method. There's no need to test it directly.
+    expect(toggle.props.toggled).toEqual(false);
   });
 });
