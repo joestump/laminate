f93274919896a89cb5d3973c392ef10ba0d3a016
Joe Stump <joe@joestump.net>
2015-01-26 22:13:16 -0800

    Added a TabBarLabel component.

---
+++ src/components/tab-bar/label.js
@@ -0,0 +1,36 @@
+var React = require('react/addons');
+
+var Label = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string.isRequired,
+    href: React.PropTypes.string.isRequired, 
+    active: React.PropTypes.bool.isRequired
+  },
+
+  getDefaultProps: function() {
+    return {
+      href: '',
+      active: false
+    };
+  },
+
+  render: function() {
+    var labelClasses = React.addons.classSet({
+      'tab-item': this.props.href.length,
+      'tabe-label': !this.props.href.length,
+      'active': this.props.active
+    });
+
+    if (this.props.href.length) {
+      return (
+        <a className={labelClasses} href={this.props.href}>{this.props.text}</a>
+      );
+    } else {
+      return (
+        <span className={labelClasses}>{this.props.text}</span>
+      );
+    }
+  }
+});
+
+module.exports = Label;
