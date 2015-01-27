081b9e4771992d535b909b4858e1a8cda7eb2648
Joe Stump <joe@joestump.net>
2015-01-26 21:57:47 -0800

    Added an initial SegmentControlItem component.

---
+++ src/components/segmented-control/item.js
@@ -0,0 +1,35 @@
+var React = require('react/addons');
+var _ = require('lodash');
+
+var SegmentedControlItem = React.createClass({
+  props: {
+    label: React.PropTypes.string.isRequired,
+    href: React.PropTypes.string,
+    active: React.PropTypes.bool.isRequired
+  },
+
+  getDefaultProps: function() {
+    return {
+      active: false
+    };
+  },
+
+  render: function() {
+    itemClasses = {
+      'control-item': true,
+      'active': this.props.acive
+    }
+
+    if (_.isString(this.props.href) && this.props.href.length) {
+      return (
+        <a className={itemClasses} href={this.props.href}>{this.props.label}</a>
+      );
+    } else {
+      return (
+        <a className={itemClasses}>{this.props.label}</a>
+      );
+    }
+  }
+});
+
+module.exports = SegmentedControlItem;
