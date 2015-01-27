8b91e501774bf48e3194c716ec5faee127ed9fd9
Joe Stump <joe@joestump.net>
2015-01-26 19:10:14 -0800

    Fixed up linting and a JSX warning in the InputGroupRow tests.

--- src/components/input-group/__tests__/row-tests.js
+++ src/components/input-group/__tests__/row-tests.js
@@ -12,7 +12,11 @@ describe('InputGroupRow', function() {
     React = require('react/addons');
     TestUtils = React.addons.TestUtils;
     InputGroupRow = require('../row');
-    inputGroupRow = <InputGroupRow label="test" />;
+    inputGroupRow = (
+      <InputGroupRow label="test">
+        <input type="text"/>
+      </InputGroupRow>
+    );
   });
 
   it('is a InputGroupRow element', function() {
@@ -21,5 +25,11 @@ describe('InputGroupRow', function() {
 
   it('is defined', function() {
     expect(InputGroupRow).toBeDefined();
+  });
+
+  it('has class input-row', function() {
+    var elem = TestUtils.renderIntoDocument(inputGroupRow);
+    var node = elem.getDOMNode();
+    expect($(node).hasClass('input-row')).toEqual(true);
   });
 });
