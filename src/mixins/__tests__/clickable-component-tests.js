788d0d1043c2d6f24b6d6753e8addb97ed777f9b
Joe Stump <joe@joestump.net>
2015-01-27 14:19:28 -0800

    Fix lint issues.

--- src/mixins/__tests__/clickable-component-tests.js
+++ src/mixins/__tests__/clickable-component-tests.js
@@ -3,11 +3,9 @@
 jest.dontMock('../clickable-component');
 
 describe('ClickableComponentMixin', function() {
-  var ClickableComponentMixin, React, TestUtils;
+  var ClickableComponentMixin;
 
   beforeEach(function() {
-    React = require('react/addons');
-    TestUtils = React.addons.TestUtils;
     ClickableComponentMixin = require('../clickable-component');
   });
 
@@ -24,13 +22,13 @@ describe('ClickableComponentMixin', function() {
   describe('getDefaultProps', function() {
     var defaultProps;
     beforeEach(function() {
-      defaultProps = ClickableComponentMixin.getDefaultProps(); 
+      defaultProps = ClickableComponentMixin.getDefaultProps();
     });
 
     it('sets href to empty string', function() {
       expect(defaultProps.href).toEqual('');
     });
-    
+
     it('sets onClick to undefined', function() {
       expect(defaultProps.onClick).toBeUndefined();
     });
